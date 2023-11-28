324f16be54ec56076199f1c5ac511ba7ca342544
kiennt90horus <kiennt90@horusvn.com>
2023-11-28 17:23:18 +0700

    modify launch guest, admin

--- webui-user-admin.bat
+++ webui-user-admin.bat
@@ -4,6 +4,6 @@ set PYTHON=
 set GIT=
 set VENV_DIR=
 set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6,max_split_size_mb:512
-set COMMANDLINE_ARGS=--precision full --no-half --lowvram --always-batch-cond-uncond --xformers
+set COMMANDLINE_ARGS=--precision full --no-half --lowvram --xformers
 
 call webui.bat
